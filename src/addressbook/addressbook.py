e893934c796cd91aab42c9654eb60c4ba1ccfeb3
VolodymyK <vkill7595@gmail.com>
2024-02-15 17:21:18 -0600

    new edits

--- src/addressbook/addressbook.py
+++ src/addressbook/addressbook.py
@@ -1,6 +1,11 @@
 from collections import UserDict
-from datetime import date
+from datetime import datetime
 
 class AddressBook(UserDict):
     def add_address(self, name, address):
-            self.data[name] = {'Address': address}
+        self.data[name] = {'Address': address}
+
+    def get_birthday_date(self):
+        if self.birthday:
+            return datetime.strptime(self.birthday, "%Y-%m-%d").date()
+        return None
--- src/addressbook/birthday.py
+++ src/addressbook/birthday.py
@@ -1,22 +1,16 @@
 from datetime import datetime, timedelta
 
-class AddressBook:
-    def __init__(self):
-        self.contacts = []
-
 class Birthday:
-    @staticmethod
     def upcoming_birthdays(address_book, days):
-        current_date = datetime.now()
+        current_date = datetime.now().date()
         upcoming_date = current_date + timedelta(days=days)
 
         upcoming_birthdays_list = []
 
-        for contact in address_book.contacts:
-            if contact.birthday is not None:
-                birthday_date = datetime(current_date.year, contact.birthday.month, contact.birthday.day)
+        for contact in address_book.values():
+            birthday_date = contact.get_birthday_date()
 
-                if current_date <= birthday_date < upcoming_date:
-                    upcoming_birthdays_list.append(contact)
+            if birthday_date and current_date <= birthday_date < upcoming_date:
+                upcoming_birthdays_list.append(contact)
 
         return upcoming_birthdays_list
