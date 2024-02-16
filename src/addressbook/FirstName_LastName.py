6a6b9859e3ecba55edbb0ec34367785cb69bbe99
yufira <fironovayu@gmail.com>
2024-02-16 10:08:42 +0200

    FirstName and LastName classes

---
+++ src/addressbook/FirstName_LastName.py
@@ -0,0 +1,57 @@
+import re
+
+
+class FirstName:
+
+    def __init__(self, value):
+        if not self.is_valid(value):
+            raise ValueError("Invalid value")
+        self.__value = value
+
+    def __str__(self):
+        return str(self.__value)
+
+    def is_valid(self, value):
+        if not re.fullmatch(r'[a-zA-Zа-яА-Я]{2,}', value):
+            return False
+        if not value.istitle():
+            return False
+        return True
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        if not self.is_valid(value):
+            raise ValueError("Invalid value")
+        self.__value = value
+
+
+class LastName:
+
+    def __init__(self, value):
+        if not self.is_valid(value):
+            raise ValueError("Invalid value")
+        self.__value = value
+
+    def __str__(self):
+        return str(self.__value)
+
+    def is_valid(self, value):
+        if not re.fullmatch(r'[a-zA-Zа-яА-Я]{2,}', value):
+            return False
+        if not value.istitle():
+            return False
+        return True
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        if not self.is_valid(value):
+            raise ValueError("Invalid value")
+        self.__value = value
