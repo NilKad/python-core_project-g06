ea86a3832538b94d43f85527bf4a734ae7c54481
VolodymyK <vkill7595@gmail.com>
2024-02-16 20:28:19 -0600

    Update address.py

--- src/addressbook/address.py
+++ src/addressbook/address.py
@@ -1,6 +1,14 @@
 class Address:
     def __init__(self, address_string):
-        self.address_string = address_string
+        self._address_string = address_string
+
+    @property
+    def address_string(self):
+        return self._address_string
+
+    @address_string.setter
+    def address_string(self, new_address):
+        self._address_string = new_address
 
     def __str__(self):
         return self.address_string
