b935e792cd79e5b6f2b9bf59b5717e3237b7a0e1
VladVizonok <vlad.vizonok@gmail.com>
2024-02-17 15:29:01 +0200

    Change and format notes

--- src/notes/notebook.py
+++ src/notes/notebook.py
@@ -1,54 +1,113 @@
-from collections import UserDict
+from collections import UserList
+from rich.console import Console
+from rich.table import Table
 
 
-class Notebook(UserDict):
-    id = 1
+
+
+class Notebook(UserList):
+    __id = 1
     def add_note(self, note):
-        self.data[self.id] = note
-        self.id += 1
+        note.ID = self.__id
+        self.data.append(note)
+        self.__id += 1
+
 
     def find_by_tag(self, tag):
         #Повертає список заголовків статей по заданому тегу
         result = []
-        for note in self.data.values():
+        for note in self.data:
             if tag in note.tag:
-                result.append(note.title)
-        return result
+                result.append(note)
+
+        contact_table = Table(title='Find by tag', show_lines=True, width=125)
+        contact_table.add_column('Tag', style='cyan')
+        contact_table.add_column('ID', style='magenta')
+        contact_table.add_column('Title', style='yellow')
+        contact_table.add_column('Text', style='green')           
+
+        for note in result:
+            contact_table.add_row(tag, str(note.ID), note.title, note.text)
+
+        console = Console()
+        console.print(contact_table)
     
+
     def sort_by_tag(self):
         #Повертає словник з де ключ це тег, а значення список із статей в яких він є
         result = {}
-        for note in self.data.values():
+        for note in self:
             for tag in note.tag:
                 if tag not in result.keys():
-                    result[tag] = [note.title]
+                    result[tag] = [note]
                 else:
-                    result[tag].append(note.title)
-        return result
-            
+                    result[tag].append(note)
+
+        contact_table = Table(title='Sort by tag', show_lines=True, width=125)
+        contact_table.add_column('Tag', style='cyan')
+        contact_table.add_column('ID', style='magenta')
+        contact_table.add_column('Title', style='yellow')
+        contact_table.add_column('Text', style='green')           
+
+        for key, value in result.items():
+            for elem in value:
+                contact_table.add_row(key, str(elem.ID), elem.title, elem.text)
+
+        console = Console()
+        console.print(contact_table)
+         
+
     def find_by_id(self, id):
-        return self.data.get(id)
+        for note in self.data:
+            if note.ID == id:
+                contact_table = Table(title='Find by ID', show_lines=True, width=125)
+                contact_table.add_column('ID', style='cyan')
+                contact_table.add_column('Tag', style='magenta')
+                contact_table.add_column('Title', style='yellow')
+                contact_table.add_column('Text', style='green')           
+
+                contact_table.add_row(str(note.ID), note.tag[0], note.title, note.text)
+
+                console = Console()
+                console.print(contact_table)
+            
 
     def find_by_title(self, title):
-        for note in self.data.values():
-            if note.title == title:
-                return note
+        result = []
+        for note in self.data:
+            if title == note.title:
+                result.append(note)
+
+        contact_table = Table(title='Find by title', show_lines=True, width=125)
+        contact_table.add_column('ID', style='cyan')
+        contact_table.add_column('Tag', style='magenta')
+        contact_table.add_column('Title', style='yellow')
+        contact_table.add_column('Text', style='green')           
+
+        for note in result:
+            contact_table.add_row(str(note.ID), note.tag[0], note.title, note.text)
+
+        console = Console()
+        console.print(contact_table)
     
-    def delete(self, id_or_title):
-        #Функція видаляє по ID або по назві записки
-        for id, note in self.data.items():
-            if id == id_or_title:
-                self.data.pop(id)
-                break
-            if note.title == id_or_title:
-                self.data.pop(id)
-                break
+    
+    def delete(self, id):
+        #Функція видаляє по ID 
+        for note in self.data:
+            if note.ID == id:
+                self.data.remove(note)
     
     def show_all_notes(self):
-        #Костиль виводу всіх записок 
-        print('|{:^10}|{:^30}|{:^30}|'.format('ID', "TEG", "TITLE"))
-        for id, note in self.data.items():
-            print('|{:^10}|{:^30}|{:^30}|'.format(id, note.tag[0], note.title))
-            
-    
+        #
+        contact_table = Table(title='All notes', show_lines=True, width=125)
+        contact_table.add_column('ID', style='cyan')
+        contact_table.add_column('Tag', style='magenta')
+        contact_table.add_column('Title', style='yellow')
+        contact_table.add_column('Text', style='green')           
+
+        for note in self.data:
+            contact_table.add_row(str(note.ID), note.tag[0], note.title, note.text)
+
+        console = Console()
+        console.print(contact_table)
     
--- src/notes/notes.py
+++ src/notes/notes.py
@@ -1,11 +1,12 @@
-
+from notebook import Notebook
 
 class Note:
     # Для того, щоб вірно додати тег, потрібно попросити користувача вводити теги через пробіл в форматі "#tag #tag"
     def __init__(self, title, text):
-        self.tag = 'No tag`s'
+        self.tag = ['No tag`s']
         self.title = title
         self.text = text
+        self.ID = None
     
     def add_tag(self, tag):
     #Якщо користувач пропустить знак '#' під час введення, програма автоматично його додасть
@@ -40,15 +41,37 @@ class Note:
             
 
 book = Notebook()
-new_note = Note('Modern Buildins', 'Modern buildings showcase a wide range of architectural styles, \ntechnologies, and sustainable design practices.')
-new_note.add_tag('buildings architecture #design, art')
+note1 = Note('Modern Buildings', 'Modern buildings showcase a wide range of architectural styles, technologies, and sustainable design practices.')
+note1.add_tag('buildings architecture #design')
 
-second_note = Note('Salvador Dali', 'Dalí was a leading figure in the Surrealist movement, \nwhich sought to unlock the creative potential of the unconscious mind.')
-second_note.add_tag('art #surrealism')
+note2 = Note('Salvador Dali', 'Dalí was a leading figure in the Surrealist movement, \nwhich sought to unlock the creative potential of the unconscious mind.')
+note2.add_tag('art #surrealism')
 
-book.add_note(new_note)
-book.add_note(second_note)
+note3 = Note('New Inventions', 'Explore the latest inventions and technological advancements that are shaping the future.')
+note3.add_tag('technology #innovation')
 
-print(book.find_by_tag('#art'))
-print(book.sort_by_tag())
+note4 = Note('Historical Events', 'Discover key events that shaped the course of history and influenced societies around the world.')
+note4.add_tag('history #events')
 
+note5 = Note('Space Exploration', 'Delve into the mysteries of outer space and learn about the latest discoveries in space exploration.')
+note5.add_tag('space #science')
+
+note6 = Note('Nature Photography', 'Experience the beauty of nature through stunning photography capturing landscapes, wildlife, and more.')
+note6.add_tag('nature #photography #art')
+
+book.add_note(note1)
+book.add_note(note2)
+book.add_note(note3)
+book.add_note(note4)
+book.add_note(note5)
+book.add_note(note6)
+
+
+
+
+
+book.show_all_notes()
+book.find_by_tag('#history')
+book.find_by_id(5)
+book.find_by_title('Nature Photography')
+book.sort_by_tag()
