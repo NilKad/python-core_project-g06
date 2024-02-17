8a20db4521c675690f42d9219cfd2b685d3fd5e5
VladVizonok <vlad.vizonok@gmail.com>
2024-02-17 18:49:02 +0200

    change edit_tags

--- src/notes/notes.py
+++ src/notes/notes.py
@@ -9,7 +9,6 @@ class Note:
         self.ID = None
     
     def add_tag(self, tag):
-    #Якщо користувач пропустить знак '#' під час введення, програма автоматично його додасть
         if self.tag == ['No tag`s']:
             self.tag = tag.split(' ')
         else:
@@ -17,11 +16,6 @@ class Note:
 
     def edit_tag(self, new_tag):
         self.tag = new_tag.split(' ')
-        for elem in self.tag:
-            if elem[0] != '#':
-                tags_elem = '#' + elem
-                self.tag.remove(elem)
-                self.tag.insert(0, tags_elem)
     
     def edit_title(self, new_title):
         self.title = new_title
@@ -43,7 +37,7 @@ note1 = Note('Modern Buildings', 'Modern buildings showcase a wide range of arch
 note1.add_tag('buildings architecture #design')
 
 note2 = Note('Salvador Dali', 'Dalí was a leading figure in the Surrealist movement, \nwhich sought to unlock the creative potential of the unconscious mind.')
-note2.add_tag('art')
+note2.add_tag('#art')
 note2.add_tag('art2')
 
 note3 = Note('New Inventions', 'Explore the latest inventions and technological advancements that are shaping the future.')
@@ -54,6 +48,7 @@ note4.add_tag('history #events')
 
 note5 = Note('Space Exploration', 'Delve into the mysteries of outer space and learn about the latest discoveries in space exploration.')
 note5.add_tag('space #science')
+note5.edit_tag('moonshine')
 
 note6 = Note('Nature Photography', 'Experience the beauty of nature through stunning photography capturing landscapes, wildlife, and more.')
 note6.add_tag('nature #photography #art')
@@ -70,7 +65,7 @@ book.add_note(note6)
 
 
 book.show_all_notes()
-book.find_by_tag('#history')
+book.find_by_tag('history')
 book.find_by_id(5)
 book.find_by_title('Nature Photography')
 book.sort_by_tag()
