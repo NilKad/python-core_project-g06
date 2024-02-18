adfaf2bec1d3508f923a373b58ee349ab879a157
yufira <fironovayu@gmail.com>
2024-02-18 20:28:51 +0200

    updated edit_records functions

--- src/addressbook/edit_record.py
+++ src/addressbook/edit_record.py
@@ -1,23 +1,44 @@
 from collections import UserDict
 
-#1st variant:
+
 class AddressBook(UserDict):
-    def edit_record(self, record, old_value, new_value):
-        if old_value in record:
-            record[old_value] = new_value
-        else:
-            raise ValueError(f"{old_value} not found in the record")
 
-
-#2nd variant:
-#records is a list of records
-class AddressBook:
-    def edit_record(self, records, identifier, identifier_value, old_value, new_value):
-        for record in records:
-            if record.get(identifier) == identifier_value:
-                if old_value in record:
-                    record[old_value] = new_value
-                else:
-                    raise ValueError(f"{old_value} not found in the record")
+    def set_first_name(self, id, value):
+        if id in self.data:
+            self.data[id]['first_name'] = value
         else:
-            raise ValueError(f"{identifier_value} not found in the address book")
+            raise KeyError(f'Record with id {id} not found')
+
+    def set_last_name(self, id, value):
+        if id in self.data:
+            self.data[id]['last_name'] = value
+        else:
+            raise KeyError(f'Record with id {id} not found')
+
+    def set_birthday(self, id, value):
+        if id in self.data:
+            self.data[id]['birthday'] = value
+        else:
+            raise KeyError(f'Record with id {id} not found')
+
+    def set_address(self, id, value):
+        if id in self.data:
+            self.data[id]['address'] = value
+        else:
+            raise KeyError(f'Record with id {id} not found')
+
+    def set_email(self, id, value):
+        if id in self.data:
+            self.data[id]['email'] = value
+        else:
+            raise KeyError(f'Record with id {id} not found')
+
+    def edit_phone(self, id, old_phone, new_phone):
+        if id in self.data:
+            if old_phone in self.data['id']['phones']:
+                self.data['id']['phones'].remove(old_phone)
+                self.data['id']['phones'].add(new_phone)
+            else:
+                raise ValueError(f'Phone number {old_phone} not found in the record with id {id}')
+        else:
+            raise KeyError(f'Record with id {id} not found')
