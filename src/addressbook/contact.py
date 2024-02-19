9f4df3cd514488fecbd12047fc09c596efed9f21
VolodymyK <vkill7595@gmail.com>
2024-02-19 11:50:48 -0600

    birthday update, contact.py update and del records

--- src/addressbook/contact.py
+++ src/addressbook/contact.py
@@ -1,30 +1,173 @@
-from edit_record import Record
+from datetime import datetime, timedelta
+import re
+
+class FirstName:
+    def __init__(self, value):
+        self.set_value(value)
+
+    def __str__(self):
+        return str(self.value)
+
+    def is_valid(self, value):
+        return re.fullmatch(r'[a-zA-Zа-яА-Я]{2,}', value) and value.istitle()
+
+    def set_value(self, value):
+        if not self.is_valid(value):
+            raise ValueError("Invalid value")
+        self.value = value
+
+class LastName:
+    def __init__(self, value):
+        self.set_value(value)
+
+    def __str__(self):
+        return str(self.value)
+
+    def is_valid(self, value):
+        return re.fullmatch(r'[a-zA-Zа-яА-Я]{2,}', value) and value.istitle()
+
+    def set_value(self, value):
+        if not self.is_valid(value):
+            raise ValueError("Invalid value")
+        self.value = value
+
+class Email:
+    def __init__(self, value):
+        self.set_value(value)
+
+    def __str__(self):
+        return str(self.value)
+
+    def is_valid(self, value):
+        return re.fullmatch(r'[a-zA-Z]{1}[\w\.]+@[a-zA-Z]+\.[a-zA-Z]{2,}', value)
+
+    def set_value(self, value):
+        if not self.is_valid(value):
+            raise ValueError("Invalid value")
+        self.value = value
+
+class Phone:
+    def __init__(self, value):
+        self.set_value(value)
+
+    def __str__(self):
+        return str(self.value)
+
+    def is_valid(self, value):
+        return re.fullmatch(r'[\d-]+', value)
+
+    def set_value(self, value):
+        if not self.is_valid(value):
+            raise ValueError("Invalid value")
+        self.value = value
+
+class Birthday:
+    def __init__(self, value):
+        self.set_value(value)
+
+    def __str__(self):
+        return str(self.value)
+
+    def is_valid(self, value):
+        try:
+            datetime.strptime(value, "%Y-%m-%d")
+            return True
+        except ValueError as e:
+            raise ValueError(f"Wrong date format: {e}")
+
+    def set_value(self, value):
+        if not self.is_valid(value):
+            raise ValueError("Invalid value")
+        self.value = value
+
+class Address:
+    def __init__(self, address_string):
+        self.address_str = address_string
+
+    @property
+    def address_string(self):
+        return self.address_str
+
+    @address_string.setter
+    def address_string(self, new_address):
+        self.address_str = new_address
+
+    def __str__(self):
+        return self.address_str
 
 class Contact:
-
     def set_first_name(self, value):
-        self.first_name = value
+        self.first_name = FirstName(value)
 
     def set_last_name(self, value):
-        self.last_name = value
+        self.last_name = LastName(value)
 
     def set_birthday(self, value):
-        self.birthday = value
+        self.birthday = Birthday(value)
 
     def set_address(self, value):
-        self.address = value
+        self.address = Address(value)
 
     def set_email(self, value):
-        self.email = value
+        self.email = Email(value)
 
     def add_phones(self, phones):
-        self.phones.extend(phones)
+        self.phones = [Phone(phone) for phone in phones]
 
     def edit_phone(self, old_phone, new_phone):
-        if old_phone in self.phones:
-            index = self.phones.index(old_phone)
-            self.phones[index] = new_phone
+        if any(old_phone == phone.value for phone in self.phones):
+            self.phones = [Phone(new_phone) if phone.value == old_phone else phone for phone in self.phones]
 
     def del_phone(self, phone):
-        if phone in self.phones:
-            self.phones.remove(phone)
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def get_birthday_date(self):
+        return self.birthday.value if self.birthday else None
+
+    def edit_first_name(self, new_first_name):
+        self.first_name.set_value(new_first_name)
+
+    def edit_last_name(self, new_last_name):
+        self.last_name.set_value(new_last_name)
+
+    def edit_birthday(self, new_birthday):
+        if new_birthday is not None and not Birthday(new_birthday).is_valid(new_birthday):
+            raise ValueError("Invalid birthday format")
+        self.birthday.set_value(new_birthday)
+
+    def edit_address(self, new_address):
+        self.address.address_string = new_address
+
+    def edit_email(self, new_email):
+        if new_email is not None and not Email(new_email).is_valid(new_email):
+            raise ValueError('Invalid email format')
+        self.email.set_value(new_email)
+
+class Record:
+    def __init__(self, first_name, last_name, birthday=None, address=None, email=None):
+        self.first_name = FirstName(first_name)
+        self.last_name = LastName(last_name)
+        self.birthday = Birthday(birthday) if birthday else None
+        self.address = Address(address) if address else None
+        self.email = Email(email) if email else None
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones = [Phone(p) for p in phone]
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_number, new_number):
+        if any(old_number == phone.value for phone in self.phones):
+            self.phones = [Phone(new_number) if phone.value == old_number else phone for phone in self.phones]
+
+    def set_email(self, email):
+        self.email = Email(email)
+
+    def __str__(self):
+        return (
+            f"Contact name: {self.first_name.value} {self.last_name.value}, "
+            f"phones: {'; '.join(p.value for p in self.phones)}, "
+            f"Birthday: {self.birthday}, Email: {self.email}, Address: {self.address}"
+        )
--- src/addressbook/contacts.py
+++ src/addressbook/contacts.py
@@ -1,9 +1,8 @@
 from datetime import datetime, timedelta
-from record import Record
+from contact import Record
 from collections import UserDict
 from contact import Contact
-from edit_record import FirstName, LastName, Email, Phone, Birthday, Address, Record
-
+from contact import FirstName, LastName, Email, Phone, Birthday, Address, Record
 
 class Contacts:
     def __init__(self):
@@ -16,7 +15,7 @@ class Contacts:
         address = data.get('address')
         birthday = data.get('birthday')
         email = data.get('email')
-        phones = data.get('phones', [])
+        phones = set(data.get('phones', []))
 
         contact = Record(first_name, last_name, address, email, birthday, phones, contact_id=self.current_id)
         contact_id = self.current_id
@@ -27,7 +26,7 @@ class Contacts:
     def delete(self, contact_id):
         if contact_id in self.contacts:
             del self.contacts[contact_id]
-            
+
     def find_by_id(self, contact_id):
         return self.contacts.get(contact_id)
 
@@ -39,19 +38,38 @@ class Contacts:
                 value.lower() in contact.last_name.value.lower() or
                 value.lower() in contact.address.address_string.lower() or
                 value.lower() in contact.email.value.lower() or
-                any(value.lower() in phone.value.lower() for phone in contact.phones)
+                any(value in phone.value for phone in contact.phones)
             ):
                 result.append((contact_id, contact))
         return result
 
-    def birthday(self, value):
+    def birthday(self, *dates):
         upcoming_birthdays_list = []
 
-        for contact_id, contact in self.contacts.items():
-            if contact.birthday:
-                birthday_date = contact.birthday.value.timetuple().tm_yday
-
-                if birthday_date == value:
-                    upcoming_birthdays_list.append((contact_id, contact))
-
+        if not dates:
+            # Scenario 1: No parameters provided
+            today_date = datetime.now().timetuple().tm_yday
+            upcoming_birthdays_list.extend(
+                (contact_id, contact)
+                for contact_id, contact in self.contacts.items()
+                if contact.birthday and contact.birthday.value.timetuple().tm_yday == today_date
+            )
+        else:
+            for date in dates:
+                if isinstance(date, datetime):
+                    # Scenario 2: Single date provided
+                    day_of_year = date.timetuple().tm_yday
+                    upcoming_birthdays_list.extend(
+                        (contact_id, contact)
+                        for contact_id, contact in self.contacts.items()
+                        if contact.birthday and contact.birthday.value.timetuple().tm_yday == day_of_year
+                    )
+                elif isinstance(date, tuple) and len(date) == 2 and isinstance(date[0], datetime) and isinstance(date[1], datetime):
+                    # Scenario 3: Two dates provided
+                    start_date, end_date = date
+                    upcoming_birthdays_list.extend(
+                        (contact_id, contact)
+                        for contact_id, contact in self.contacts.items()
+                        if contact.birthday and start_date.timetuple().tm_yday <= contact.birthday.value.timetuple().tm_yday <= end_date.timetuple().tm_yday
+                    )
         return upcoming_birthdays_list
