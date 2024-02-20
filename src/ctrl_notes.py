0ee38783848c2f9dd50af2f67eb5d187198ff77b
NilKad <kadulin@gmail.com>
2024-02-20 11:36:02 +0100

    ctrl params

--- src/ctrl_notes.py
+++ src/ctrl_notes.py
@@ -1,45 +1,61 @@
 def note_add(*args, **kwargs):
+    params, *_ = args
     print(f"note_add args: {args}")
+    print(f"note_add args: {params}")
     print(f"note_add kwargs: {kwargs}")
 
     return "Note add OK"
 
 
 def note_set(*args, **kwargs):
+    params, *_ = args
     print(f"note_add args: {args}")
+    print(f"note_add args: {params}")
     print(f"note_add kwargs: {kwargs}")
 
     return "Note edit OK"
 
+
 def note_del(*args, **kwargs):
+    params, *_ = args
     print(f"note_add args: {args}")
+    print(f"note_add args: {params}")
     print(f"note_add kwargs: {kwargs}")
 
     return "Note edit OK"
 
 
 def note_find(*args, **kwargs):
+    params, *_ = args
     print(f"note_add args: {args}")
+    print(f"note_add args: {params}")
     print(f"note_add kwargs: {kwargs}")
 
     return "Note edit OK"
 
 
 def note_findsubject(*args, **kwargs):
+    params, *_ = args
     print(f"note_add args: {args}")
+    print(f"note_add args: {params}")
     print(f"note_add kwargs: {kwargs}")
 
     return "Note edit OK"
 
 
 def note_sort_by_tag(*args, **kwargs):
+    params, *_ = args
     print(f"note_add args: {args}")
+    print(f"note_add args: {params}")
     print(f"note_add kwargs: {kwargs}")
 
     return "Note edit OK"
 
+
 def note_show(*args, **kwargs):
+    params, *_ = args
     print(f"note_add args: {args}")
+    print(f"note_add args: {params}")
     print(f"note_add kwargs: {kwargs}")
 
     return "Note edit OK"
